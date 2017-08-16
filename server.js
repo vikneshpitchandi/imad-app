b2885c7e3ada5301dc119bf2fceca2422b64ee65
vikneshpitchandi <30996993+vikneshpitchandi@users.noreply.github.com>
2017-08-16 20:43:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article2',function(req,res){
+app.get('/article1',function(req,res){
     res.sendFile(path.join(--dirname,'ui','article-one.html'));
 }
 );
