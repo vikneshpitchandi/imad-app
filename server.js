5f231363dbf038acd99a379a236358b76e517855
vikneshpitchandi <30996993+vikneshpitchandi@users.noreply.github.com>
2017-08-21 20:29:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ function createtemplate(data) // createtemplate is a function which is to be cal
 <head>
     <title> ${title}</title>
     <meta name='viewport' content="width=device=width ,initial-scale=1"/>
-    <link href="/ui/style.css"  rel="stylesheet"/>
+    <link href="ui/style.css"  rel="stylesheet"/>
     </head>
     
     <body>
