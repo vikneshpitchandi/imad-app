052eda3af34059bb638800fe7c609e8b7a099833
vikneshpitchandi <30996993+vikneshpitchandi@users.noreply.github.com>
2017-08-21 20:57:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
 console.log('Loaded!');
+
+//change the main-text div in index.html 
+
+var element=document.getElementById('main-text');
+
+element.innerHTML='new value';
+
+//move the image
+var img=document.getElementById('madi');
+var marginleft=0;
+function moveright()
+{
+    marginleft=marginleft+10;
+    img.style.marginleft=marginleft+'px';
+}
+img.onclick=function()
+{
+    var interval=setInterval(moveright,50);
+};
