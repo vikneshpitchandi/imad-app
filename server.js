4e6d1b9966b58b64b2cb7c582505da35e7165f37
vikneshpitchandi <30996993+vikneshpitchandi@users.noreply.github.com>
2017-08-22 06:45:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,11 +103,13 @@ app.get('/:articleName',function(req,res)   // /articleName refers to the html,c
     
 });
 
-var counter=0;
-app.get('/counter',function(req,res){
+
+counter=counter+1;
+app.get('/counter', function (req, res) {
     counter=counter+1;
-    res.send(counter.toString());
+  res.send(counter.toString());
 });
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
